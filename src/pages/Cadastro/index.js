ad1172ad58ef83dbbd8cea31ed289fe65a617346
joao <joaoreis1@unipam.edu.br>
2021-03-28 15:00:05 -0300

    cadastro de cliente no firestore

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -8,11 +8,26 @@ import style from './styles'
 import firebase from '../../services/firebaseConection'
 
 import CustomHeader from '../../components/Header'
+import { ScrollView } from 'react-native'
 
 function Cadastro({ navigation }) {
 
+    const [nome, setNome] = useState('')
+
+    const [ddd, setDdd] = useState('')
+    const [telefone, setTelefone] = useState('')
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const [cep, setCep] = useState('')
+    const [logradouro, setLogradouro] = useState('')
+    const [numero, setNumero] = useState('')
+    const [complemento, setComplemento] = useState('')
+    const [bairro, setBairro] = useState('')
+    const [cidade, setCidade] = useState('')
+    const [estado, setEstado] = useState('')
+
 
     const onChangeEmail = (txtEmail) => {
         setEmail(txtEmail)
@@ -22,23 +37,82 @@ function Cadastro({ navigation }) {
         setPassword(txtPassword)
     }
 
+    const onChangeNome = (txtNome) => {
+        setNome(txtNome)
+    }
+
+    const onChangeTelefone = (txtTelefone) => {
+        setTelefone(txtTelefone)
+    }
+
+    const onChangeDdd = (txtDdd) => {
+        setDdd(txtDdd)
+    }
+
+    const onChangeLogradouro = (txtLogradouro) => {
+        setLogradouro(txtLogradouro)
+    }
+
+    const onChangeNumero = (txtNumero) => {
+        setNumero(txtNumero)
+    }
+
+    const onChangeComplemento = (txtComplemento) => {
+        setComplemento(txtComplemento)
+    }
+
+    const onChangeBairro = (txtBairro) => {
+        setBairro(txtBairro)
+    }
+
+    const onChangeCidade = (txtCidade) => {
+        setCidade(txtCidade)
+    }
+
+    const onChangeEstado = (txtEstado) => {
+        setEstado(txtEstado)
+    }
+
+    const onChangeCep = (txtCep) => {
+        setCep(txtCep)
+    }
+
+    const buscarCep = (cep) => {
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then(res => res.json())
+            .then((data) => {
+                setLogradouro(data.logradouro)
+                setComplemento(data.complemento)
+                setBairro(data.bairro)
+                setCidade(data.localidade)
+                setEstado(data.uf)
+            })
+            .catch((err) => alert(err))
+    }
+
     const Cadastration = () => {
+        // criar usuario no auth do firebase
         firebase.auth().createUserWithEmailAndPassword(email, password).then(
             (data) => {
                 const usuarioId = data.user.uid
+
+                // criar usuario no firestore
                 firebase.firestore().collection('usuario').doc(usuarioId).set({
-                    nome: "nome",
+                    nome: nome,
                     email: email,
-                    telefone: '99999999',
+                    telefone: ddd + telefone,
                     endereco: {
-                        logradouro: 'rua',
-                        numero: '000',
-                        bairro: '',
-                        cidade: '',
-                        estado: '',
+                        logradouro: logradouro,
+                        numero: numero,
+                        complemento: complemento,
+                        bairro: bairro,
+                        cidade: cidade,
+                        estado: estado,
                     }
                 })
-                navigation.navigate('Success')
+             
+                // navigation.navigate('Success')
+                navigation.navigate('Padaria')
             }).catch((err) => {
                 alert(err)
             })
@@ -47,19 +121,52 @@ function Cadastro({ navigation }) {
     return (
         <View style={{ flex: 1 }}>
             <CustomHeader navigation={navigation} title={"Cadastro de Usuários"} />
+            <ScrollView style={{ flex: 1 }}>
+                <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#DCDCDC', }}>
 
-            <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#DCDCDC', }}>
+                    <Text style={style.text}>Nome</Text>
+                    <TextInput style={style.input} value={nome} onChangeText={(txtNome) => onChangeNome(txtNome)} />
 
-                <Text style={style.text}>Email</Text>
+                    <Text style={style.text}>DDD + Telefone</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        <TextInput style={[style.input, { width: '30%' }]} value={ddd} onChangeText={(txtDdd) => onChangeDdd(txtDdd)} />
+                        <TextInput style={[style.input, { width: '70%' }]} value={telefone} onChangeText={(txtTelefone) => onChangeTelefone(txtTelefone)} />
+                    </View>
 
-                <TextInput style={{ alignItems: 'center', backgroundColor: 'white', textAlign: 'center', borderRadius: 50, borderWidth: 7, borderColor: '#DCDCDC', width: 350 }} value={email} onChangeText={(txtEmail) => onChangeEmail(txtEmail)} />
+                    <Text style={style.text}>Email</Text>
+                    <TextInput style={style.input} value={email} onChangeText={(txtEmail) => onChangeEmail(txtEmail)} />
 
-                <Text style={style.text}>Senha</Text>
-                <TextInput secureTextEntry={true} style={{ backgroundColor: 'white', textAlign: 'center', borderRadius: 50, borderWidth: 7, borderColor: '#DCDCDC', width: 350 }} value={email} onChangeText={(txtEmail) => onChangeEmail(txtEmail)} value={password} onChangeText={(txtPassword) => onChangePassword(txtPassword)} />
+                    <Text style={style.text}>Senha</Text>
+                    <TextInput secureTextEntry={true} style={style.input} value={password} onChangeText={(txtPassword) => onChangePassword(txtPassword)} />
 
-                <Button buttonStyle={style.button} title="Cadastrar" onPress={Cadastration} />
+                    <Text style={style.text}>Buscar Cep</Text>
+                    <TextInput style={style.input} value={cep} onChangeText={(txtCep) => onChangeCep(txtCep)} onBlur={() => buscarCep(cep)} />
 
-            </View >
+                    <Text style={style.text}>Logradouro</Text>
+                    <TextInput style={style.input} value={logradouro} onChangeText={(textLogradouro) => onChangeLogradouro(textLogradouro)} />
+
+                    <Text style={style.text}>Número</Text>
+                    <TextInput style={style.input} value={numero} onChangeText={(textNumero) => onChangeNumero(textNumero)} />
+
+                    <Text style={style.text}>Complemento</Text>
+                    <TextInput style={style.input} value={complemento} onChangeText={(textComplemento) => onChangeComplemento(textComplemento)} />
+
+                    <Text style={style.text}>Bairro</Text>
+                    <TextInput style={style.input} value={bairro} onChangeText={(textBairro) => onChangeBairro(textBairro)} />
+
+                    <Text style={style.text}>Cidade</Text>
+                    <TextInput style={style.input} value={cidade} onChangeText={(textCidade) => onChangeCidade(textCidade)} />
+
+
+                    <Text style={style.text}>Estado </Text>
+                    <TextInput style={style.input} value={estado} onChangeText={(textEstado) => onChangeEstado(textEstado)} />
+
+
+                    <Button buttonStyle={style.button} title="Cadastrar" onPress={Cadastration} />
+
+                </View >
+            </ScrollView>
+
         </View>
     )
 
--- src/pages/Cadastro/styles.js
+++ src/pages/Cadastro/styles.js
@@ -15,14 +15,36 @@ const style = StyleSheet.create({
 
     },
 
+    buttonBuscarCep: {
+        backgroundColor: 'red',
+        borderRadius: 20,
+        borderWidth: 7,
+        borderColor: 'red',
+        width: 300,
+        marginTop: 20,
+        marginLeft: 30,
+        justifyContent: 'center',
+        marginBottom: 10,
+
+    },
+
     text: {
         marginLeft: 10,
-        margin: 50,
+        margin: 30,
         marginBottom: -5,
         color: '#0b2031',
         fontSize: 20,
         fontWeight: 'bold'
 
+    },
+
+    input: {
+        backgroundColor: 'white',
+        textAlign: 'center',
+        borderRadius: 50,
+        borderWidth: 7,
+        borderColor: '#DCDCDC',
+        width: '100%'
     },
 
     textCad: {
